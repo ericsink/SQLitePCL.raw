3ab726aab6dc1d5bc658807c800db674ebc6ee41
Eric Sink <eric@zumero.com>
2014-04-14 16:25:04 -0500

    added Test project for the WP81.SL platform assembly

---
+++ SQLitePCL.Test.VS.WP81.SL/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SQLitePCL.Test.VS.WP81.SL.Resources;
+
+namespace SQLitePCL.Test.VS.WP81.SL
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
