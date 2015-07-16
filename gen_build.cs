b6b9bd2dc4fa50b360f91cdbde0ed631098a4e8e
Eric Sink <eric@zumero.com>
2015-07-16 12:46:08 -0500

    version num

--- gen_build.cs
+++ gen_build.cs
@@ -2757,7 +2757,7 @@ public static class gen
 		f.WriteEndElement(); // file
 	}
 
-	private const string NUSPEC_VERSION = "0.8.1-pre2";
+	private const string NUSPEC_VERSION = "0.8.1";
 	private const string NUSPEC_RELEASE_NOTES = "Update SQLite to 3.8.10.2.  Update OpenSSL libcrypto to 1.0.1p.  Add set_authorizer feature.  Add support for Xamarin.Mac.  Keep track of hooks with a ConcurrentDictionary where possible.  On Windows, when dynamic DLL load fails, fallback to normal approach.";
 
 	private static void gen_nuspec_basic(string top, string root, string id)
