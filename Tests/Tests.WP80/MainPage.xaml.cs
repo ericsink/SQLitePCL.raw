15de7fa23f0aae2a949fa78cb94a45c279e5a0c8
Eric Sink <eric@zumero.com>
2016-08-21 15:51:05 -0500

    Revert "add project.json and xunit.runner.devices to Tests.WP80, which reproduces the problem where it won't run.  so this has nothing to do with SQLitePCL.raw stuff."
    This reverts commit 4171504cae09444908aa768fff04bbd0789485d6.

--- Tests/Tests.WP80/MainPage.xaml.cs
+++ Tests/Tests.WP80/MainPage.xaml.cs
@@ -1,26 +1,41 @@
-﻿using System.Reflection;
-using Xunit.Runners.UI;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Tests.WP80.Resources;
 
 namespace Tests.WP80
 {
-    public partial class MainPage : RunnerApplicationPage
+    public partial class MainPage : PhoneApplicationPage
     {
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
         }
 
-        protected override void OnInitializeRunner()
-        {
-            
-            // tests can be inside the main assembly
-            AddTestAssembly(Assembly.GetExecutingAssembly());
-            // otherwise you need to ensure that the test assemblies will 
-            // become part of the app bundle
-            //AddTestAssembly(typeof(PortableTests).Assembly);
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
 
-        }
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
     }
 }
-
