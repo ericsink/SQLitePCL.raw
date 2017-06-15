26864e2d755a452de31b4b9d5694c12143d5d66b
Eric Sink <eric@sourcegear.com>
2017-06-15 16:04:53 -0500

    Revert "untested fix for #162, the first of the two ideas suggested by @natemcmaster"
    This reverts commit 46e5a83b0ed20ae21c63f52df9de35e9c99ae023.

--- gen_build.cs
+++ gen_build.cs
@@ -3019,11 +3019,11 @@ public static class gen
 		f.WriteEndElement(); // file
 	}
 
-	public static string NUSPEC_VERSION = string.Format("1.1.6-pre{0}", DateTime.Now.ToString("yyyyMMddHHmmss")); 
+	//public static string NUSPEC_VERSION = string.Format("1.1.5-pre{0}", DateTime.Now.ToString("yyyyMMddHHmmss")); 
 	//public static string NUSPEC_VERSION = "1.0.0-PLACEHOLDER";
-	//public static string NUSPEC_VERSION = "1.1.6";
+	public static string NUSPEC_VERSION = "1.1.5";
 
-	private const string NUSPEC_RELEASE_NOTES = "1.1.6:  AssetTargetFallback fixes.  1.1.5:  bug fix path in lib.foo.linux targets file.  1.1.4:  tweak use of nuget .targets files for compat with .NET Core.  1.1.3:  add SQLITE_CHECKPOINT_TRUNCATE symbol definition.  add new blob overloads to enable better performance in certain cases.  chg winsqlite3 to use StdCall.  fix targets files for better compat with VS 2017 nuget pack.  add 32-bit linux build for e_sqlite3.  update to latest libcrypto builds from couchbase folks.  1.1.2:  ability to FreezeProvider().  update e_sqlite3 builds to 3.16.1.  1.1.1:  add support for config_log.  update e_sqlite3 builds to 3.15.2.  fix possible memory corruption when using prepare_v2() with multiple statements.  better errmsg from ugly.step().  add win8 dep groups in bundles.  fix batteries_v2.Init() to be 'last call wins' like the v1 version is.  chg raw.SetProvider() to avoid calling sqlite3_initialize() so that sqlite3_config() can be used.  better support for Xamarin.Mac.  1.1.0:  fix problem with winsqlite3 on UWP.  remove iOS Classic support.  add sqlite3_enable_load_extension.  add sqlite3_config/initialize/shutdown.  add Batteries_V2.Init().  1.0.1:  fix problem with bundle_e_sqlite3 on iOS.  fix issues with .NET Core.  add bundle_sqlcipher.  1.0.0 release:  Contains minor breaking changes since 0.9.x.  All package names now begin with SQLitePCLRaw.  Now supports netstandard.  Fixes for UWP and Android N.  Change all unit tests to xunit.  Support for winsqlite3.dll and custom SQLite builds.";
+	private const string NUSPEC_RELEASE_NOTES = "1.1.5:  bug fix path in lib.foo.linux targets file.  1.1.4:  tweak use of nuget .targets files for compat with .NET Core.  1.1.3:  add SQLITE_CHECKPOINT_TRUNCATE symbol definition.  add new blob overloads to enable better performance in certain cases.  chg winsqlite3 to use StdCall.  fix targets files for better compat with VS 2017 nuget pack.  add 32-bit linux build for e_sqlite3.  update to latest libcrypto builds from couchbase folks.  1.1.2:  ability to FreezeProvider().  update e_sqlite3 builds to 3.16.1.  1.1.1:  add support for config_log.  update e_sqlite3 builds to 3.15.2.  fix possible memory corruption when using prepare_v2() with multiple statements.  better errmsg from ugly.step().  add win8 dep groups in bundles.  fix batteries_v2.Init() to be 'last call wins' like the v1 version is.  chg raw.SetProvider() to avoid calling sqlite3_initialize() so that sqlite3_config() can be used.  better support for Xamarin.Mac.  1.1.0:  fix problem with winsqlite3 on UWP.  remove iOS Classic support.  add sqlite3_enable_load_extension.  add sqlite3_config/initialize/shutdown.  add Batteries_V2.Init().  1.0.1:  fix problem with bundle_e_sqlite3 on iOS.  fix issues with .NET Core.  add bundle_sqlcipher.  1.0.0 release:  Contains minor breaking changes since 0.9.x.  All package names now begin with SQLitePCLRaw.  Now supports netstandard.  Fixes for UWP and Android N.  Change all unit tests to xunit.  Support for winsqlite3.dll and custom SQLite builds.";
 
     private static void add_dep_core(XmlWriter f)
     {
@@ -3178,18 +3178,26 @@ public static class gen
 			switch (cfg.toolset) {
 				case "v110_xp":
 					tname = gen_nuget_targets_pinvoke_anycpu(top, cfg.get_id(), cfg.toolset);
+                    if (tname != null) 
+                    {
+                        f.WriteStartElement("file");
+                        f.WriteAttributeString("src", tname);
+                        f.WriteAttributeString("target", string.Format("build\\net35\\{0}.targets", id));
+                        f.WriteEndElement(); // file
+                    }
 					break;
 				default:
 					tname = gen_nuget_targets_sqlite3_itself(top, cfg.get_id(), cfg.toolset);
+                    if (tname != null) 
+                    {
+                        f.WriteStartElement("file");
+                        f.WriteAttributeString("src", tname);
+                        f.WriteAttributeString("target", string.Format("build\\{0}.targets", id));
+                        f.WriteEndElement(); // file
+                    }
 					break;
 			}
 
-			if (tname != null) {
-				f.WriteStartElement("file");
-				f.WriteAttributeString("src", tname);
-				f.WriteAttributeString("target", string.Format("build\\{0}.targets", id));
-				f.WriteEndElement(); // file
-			}
 
 			f.WriteEndElement(); // files
 
@@ -3422,7 +3430,7 @@ public static class gen
 
             f.WriteStartElement("file");
             f.WriteAttributeString("src", tname);
-            f.WriteAttributeString("target", string.Format("build\\{0}.targets", id));
+            f.WriteAttributeString("target", string.Format("build\\net35\\{0}.targets", id));
             f.WriteEndElement(); // file
 
 			f.WriteEndElement(); // files
@@ -3513,7 +3521,7 @@ public static class gen
 			}
 			f.WriteStartElement("file");
 			f.WriteAttributeString("src", tname);
-			f.WriteAttributeString("target", string.Format("build\\{0}.targets", id));
+			f.WriteAttributeString("target", string.Format("build\\net35\\{0}.targets", id));
 			f.WriteEndElement(); // file
 
 			f.WriteEndElement(); // files
@@ -4266,7 +4274,6 @@ public static class gen
 			var guid = Guid.NewGuid().ToString();
 			f.WriteStartElement("Target");
 			f.WriteAttributeString("Name", string.Format("InjectReference_{0}", guid));
-			f.WriteAttributeString("Condition", " '$(TargetFrameworkIdentifier)' != '.NETCoreApp' AND '$(TargetFrameworkIdentifier)' != '.NETStandard' ");
 			f.WriteAttributeString("BeforeTargets", "ResolveAssemblyReferences");
 
 			f.WriteStartElement("ItemGroup");
@@ -4279,7 +4286,7 @@ public static class gen
 				}
 
 				f.WriteStartElement("Content");
-				f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\{0}", Path.Combine(other.get_nuget_target_path(), "e_sqlite3.dll")));
+				f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\..\\{0}", Path.Combine(other.get_nuget_target_path(), "e_sqlite3.dll")));
 				// TODO condition/exists ?
 				f.WriteElementString("Link", string.Format("{0}\\e_sqlite3.dll", other.cpu.ToLower()));
 				f.WriteElementString("CopyToOutputDirectory", "PreserveNewest");
@@ -4325,14 +4332,14 @@ public static class gen
 			f.WriteAttributeString("Condition", " '$(OS)' == 'Windows_NT' ");
 
 			f.WriteStartElement("Content");
-			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\runtimes\\win7-x86\\native\\{0}", filename));
+			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\..\\runtimes\\win7-x86\\native\\{0}", filename));
 			f.WriteElementString("Link", string.Format("{0}\\{1}", "x86", filename));
 			f.WriteElementString("CopyToOutputDirectory", "PreserveNewest");
             f.WriteElementString("Pack", "false");
 			f.WriteEndElement(); // Content
 
 			f.WriteStartElement("Content");
-			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\runtimes\\win7-x64\\native\\{0}", filename));
+			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\..\\runtimes\\win7-x64\\native\\{0}", filename));
 			f.WriteElementString("Link", string.Format("{0}\\{1}", "x64", filename));
 			f.WriteElementString("CopyToOutputDirectory", "PreserveNewest");
             f.WriteElementString("Pack", "false");
@@ -4376,7 +4383,7 @@ public static class gen
 			f.WriteAttributeString("Condition", " '$(OS)' == 'Unix' AND Exists('/Library/Frameworks') ");
 
 			f.WriteStartElement("Content");
-			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\runtimes\\osx-x64\\native\\{0}", filename));
+			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\..\\runtimes\\osx-x64\\native\\{0}", filename));
 			f.WriteElementString("Link", filename);
 			f.WriteElementString("CopyToOutputDirectory", "PreserveNewest");
             f.WriteElementString("Pack", "false");
@@ -4421,21 +4428,21 @@ public static class gen
 
 #if TODO // load library before dllimport doesn't seem to work on linux
 			f.WriteStartElement("Content");
-			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\runtimes\\linux-x64\\native\\{0}", filename));
+			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\..\\runtimes\\linux-x64\\native\\{0}", filename));
 			f.WriteElementString("Link", string.Format("{0}\\{1}", "x64", filename));
 			f.WriteElementString("CopyToOutputDirectory", "PreserveNewest");
             f.WriteElementString("Pack", "false");
 			f.WriteEndElement(); // Content
 
 			f.WriteStartElement("Content");
-			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\runtimes\\linux-x86\\native\\{0}", filename));
+			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\..\\runtimes\\linux-x86\\native\\{0}", filename));
 			f.WriteElementString("Link", string.Format("{0}\\{1}", "x86", filename));
 			f.WriteElementString("CopyToOutputDirectory", "PreserveNewest");
             f.WriteElementString("Pack", "false");
 			f.WriteEndElement(); // Content
 #else
 			f.WriteStartElement("Content");
-			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\runtimes\\linux-x64\\native\\{0}", filename));
+			f.WriteAttributeString("Include", string.Format("$(MSBuildThisFileDirectory)..\\..\\runtimes\\linux-x64\\native\\{0}", filename));
 			f.WriteElementString("Link", filename);
 			f.WriteElementString("CopyToOutputDirectory", "PreserveNewest");
             f.WriteElementString("Pack", "false");
