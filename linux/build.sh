236efcf7fc5af5d7eb201c796b86e5f321432d2d
Eric Sink <eric@sourcegear.com>
2018-02-23 14:44:24 -0600

    modified linux/build.sh to create an arm64 linux build of e_sqlite3, which I *think* is what #199 might be requesting

--- linux/build.sh
+++ linux/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -x
-echo Building armhf
 DEFS='-DNDEBUG -DSQLITE_DEFAULT_FOREIGN_KEYS=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS4 -DSQLITE_ENABLE_COLUMN_METADATA -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_RTREE '
+echo Building arm64
+aarch64-linux-gnu-gcc -shared -fPIC -O $DEFS -o arm64/libe_sqlite3.so ../sqlite3/sqlite3.c 
+echo Building armhf
 arm-linux-gnueabihf-gcc -shared -fPIC -O $DEFS -o armhf/libe_sqlite3.so ../sqlite3/sqlite3.c 
 echo Building armsf
 arm-linux-gnueabi-gcc -shared -fPIC -O $DEFS-o armsf/libe_sqlite3.so ../sqlite3/sqlite3.c 
@@ -14,3 +16,4 @@ musl-gcc -m64 -shared -fPIC -O $DEFS-o musl-x64/libe_sqlite3.so ../sqlite3/sqlit
 echo Done
 # sudo apt-get install gcc-arm-linux-gnueabihf
 # sudo apt-get install musl-dev musl-tools
+# sudo apt-get install gcc-aarch64-linux-gnu
