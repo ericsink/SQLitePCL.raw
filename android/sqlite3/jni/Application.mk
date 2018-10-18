2310a0d75185d8a547db10edcad42b2db7b87e60
Eric Sink <eric@sourcegear.com>
2018-10-18 10:19:37 -0500

    rm all the old stuff from previous ways of building sqlite3 and sqlcipher.  all that stuff is now in the cb sister repo.

--- android/sqlite3/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI := armeabi x86 x86_64 arm64-v8a armeabi-v7a
-#APP_ABI := armeabi x86 x86_64 arm64-v8a armeabi-v7a 
-#APP_ABI := all
--- apple/libs/mac/cp_mac.ps1
+++
@@ -1,5 +0,0 @@
-mkdir ../../../bld/release/bin/pcl/unified_mac
-cp -r ./pinvoke_sqlite3 ../../../bld/release/bin/pcl/unified_mac
-cp -r ./pinvoke_packaged_sqlite3 ../../../bld/release/bin/pcl/unified_mac
-cp -r ./pinvoke_packaged_sqlcipher ../../../bld/release/bin/pcl/unified_mac
-ls ../../../bld/release/bin/pcl/unified_mac
