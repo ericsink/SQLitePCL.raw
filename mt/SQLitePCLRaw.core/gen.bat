3d8c61cc82a0f282f2828361ad9669d8b51b79c2
Eric Sink <eric@sourcegear.com>
2019-02-26 16:27:49 -0600

    chg provider.tt to restore its ability to generate a DllImport version as well as the dynamic-load delegates version.  not used yet.  no chg in gen output.

--- mt/SQLitePCLRaw.core/gen.bat
+++ mt/SQLitePCLRaw.core/gen.bat
@@ -1,3 +1,3 @@
-t4 -o Generated\provider_cdecl.cs -p:CONV=Cdecl provider.tt
-t4 -o Generated\provider_stdcall.cs -p:CONV=StdCall provider.tt
+t4 -o Generated\provider_cdecl.cs -p:NAME=Cdecl -p:CONV=Cdecl -p:KIND=dynamic provider.tt
+t4 -o Generated\provider_stdcall.cs -p:NAME=StdCall -p:CONV=StdCall -p:KIND=dynamic provider.tt
 
