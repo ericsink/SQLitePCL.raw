ad427b47e94e4f822dbc1095fe57c9cb5d712ec0
Eric Sink <eric@sourcegear.com>
2019-02-27 10:16:01 -0600

    gen a provider for dllimport e_sqlite3

--- gen.bat
+++ gen.bat
@@ -1,3 +1,4 @@
 t4 -o mt\SQLitePCLRaw.provider.dynamic\Generated\provider_cdecl.cs -p:NAME=Cdecl -p:CONV=Cdecl -p:KIND=dynamic src\cs\provider.tt
 t4 -o mt\SQLitePCLRaw.provider.dynamic\Generated\provider_stdcall.cs -p:NAME=StdCall -p:CONV=StdCall -p:KIND=dynamic src\cs\provider.tt
+t4 -o mt\SQLitePCLRaw.provider.e_sqlite3\Generated\provider_e_sqlite3.cs -p:NAME=e_sqlite3 -p:CONV=Cdecl -p:KIND=dllimport -p:NAME_FOR_DLLIMPORT=e_sqlite3 src\cs\provider.tt
 
