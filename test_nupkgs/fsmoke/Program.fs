3b6101a722a09a9db3686228b78abbf1e2a432cf
Eric Sink <eric@sourcegear.com>
2019-03-03 09:57:00 -0600

    fs version of smoke

---
+++ test_nupkgs/fsmoke/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open SQLitePCL.Ugly
+
+[<EntryPoint>]
+let main argv =
+    SQLitePCL.Batteries_V2.Init()
+    use db = ugly.``open``(":memory:")
+    let s = db.query_scalar<string>("SELECT sqlite_version()")
+    Console.WriteLine("{0}", s)
+    0 // return an integer exit code
