60720fe6cc7e81e272627de14d26a61ca3205afe
Eric Sink <eric@sourcegear.com>
2019-03-05 06:46:26 -0600

    chg the version back to the one without the timestamp.  as expected, modifies Directory.Build.props and pack.bat, but not the nuspecs.

--- nuspecs/pack.bat
+++ nuspecs/pack.bat
@@ -1,15 +1,15 @@
 mkdir empty
-..\nuget pack -properties version=2.0.0-pre20190305063859 -OutputDirectory ..\nupkgs SQLitePCLRaw.lib.e_sqlite3.nuspec
+..\nuget pack -properties version=2.0.0-pre -OutputDirectory ..\nupkgs SQLitePCLRaw.lib.e_sqlite3.nuspec
 if %errorlevel% neq 0 exit /b %errorlevel%
-..\nuget pack -properties version=2.0.0-pre20190305063859 -OutputDirectory ..\nupkgs SQLitePCLRaw.lib.e_sqlcipher.nuspec
+..\nuget pack -properties version=2.0.0-pre -OutputDirectory ..\nupkgs SQLitePCLRaw.lib.e_sqlcipher.nuspec
 if %errorlevel% neq 0 exit /b %errorlevel%
-..\nuget pack -properties version=2.0.0-pre20190305063859 -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_green.nuspec
+..\nuget pack -properties version=2.0.0-pre -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_green.nuspec
 if %errorlevel% neq 0 exit /b %errorlevel%
-..\nuget pack -properties version=2.0.0-pre20190305063859 -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_e_sqlite3.nuspec
+..\nuget pack -properties version=2.0.0-pre -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_e_sqlite3.nuspec
 if %errorlevel% neq 0 exit /b %errorlevel%
-..\nuget pack -properties version=2.0.0-pre20190305063859 -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_e_sqlcipher.nuspec
+..\nuget pack -properties version=2.0.0-pre -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_e_sqlcipher.nuspec
 if %errorlevel% neq 0 exit /b %errorlevel%
-..\nuget pack -properties version=2.0.0-pre20190305063859 -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_zetetic.nuspec
+..\nuget pack -properties version=2.0.0-pre -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_zetetic.nuspec
 if %errorlevel% neq 0 exit /b %errorlevel%
-..\nuget pack -properties version=2.0.0-pre20190305063859 -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_winsqlite3.nuspec
+..\nuget pack -properties version=2.0.0-pre -OutputDirectory ..\nupkgs SQLitePCLRaw.bundle_winsqlite3.nuspec
 if %errorlevel% neq 0 exit /b %errorlevel%
