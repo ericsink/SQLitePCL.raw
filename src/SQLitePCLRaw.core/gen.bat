919f89f98820b2de271865d0a491a6f54c9f8e89
Eric Sink <eric@sourcegear.com>
2019-03-05 06:10:43 -0600

    rm old version of gen.bat I found lying around

--- src/SQLitePCLRaw.core/gen.bat
+++
@@ -1,3 +0,0 @@
-t4 -o Generated\provider_cdecl.cs -p:NAME=Cdecl -p:CONV=Cdecl -p:KIND=dynamic provider.tt
-t4 -o Generated\provider_stdcall.cs -p:NAME=StdCall -p:CONV=StdCall -p:KIND=dynamic provider.tt
-
