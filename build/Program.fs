cd7731758e424f9049223f4934b30fa778dd7a7e
Eric Sink <eric@sourcegear.com>
2019-03-06 16:22:15 -0600

    reimplement build.bat as a 'script' to run under .net core by writing a little wrapper around System.Diagnostics.Process.  much nicer and more powerful language, and closer to getting it working xplat.

---
+++ build/Program.fs
@@ -0,0 +1,131 @@
+﻿
+open System
+open System.Diagnostics
+open System.IO
+open System.Xml.Linq
+open System.Linq
+
+let exec = build.exec
+
+[<EntryPoint>]
+let main argv =
+    let timer = Stopwatch.StartNew()
+
+    let top =
+        let cwd = Directory.GetCurrentDirectory()
+        Path.GetFullPath(Path.Combine(cwd, ".."))
+        // TODO maybe walk upward until we find the right directory
+
+    exec "dotnet" "run .." (Path.Combine(top, "gen_nuspecs"))
+
+    let dir_nupkgs = Path.Combine(top, "nupkgs")
+    for s in Directory.GetFiles(dir_nupkgs, "*.nupkg") do
+        File.Delete(s)
+
+    let gen_provider dir_basename (name:string) conv kind =
+        let path_tt = Path.Combine(top, "src", "common", "provider.tt")
+        let dir_name = sprintf "SQLitePCLRaw.provider.%s" dir_basename
+        let cs_name = sprintf "provider_%s.cs" (name.ToLower())
+        let cs_path = Path.Combine(top, "src", dir_name, "Generated", cs_name)
+        let dllimport_name_arg = 
+            if kind = "dynamic" 
+            then "" 
+            else 
+                if name = "internal"
+                then (sprintf "-p:NAME_FOR_DLLIMPORT=%s" "__Internal")
+                else (sprintf "-p:NAME_FOR_DLLIMPORT=%s" name)
+        let args = sprintf "-o %s -p:NAME=%s -p:CONV=%s -p:KIND=%s %s %s" cs_path name conv kind dllimport_name_arg path_tt
+        exec "t4" args "."
+
+    gen_provider "dynamic" "Cdecl" "Cdecl" "dynamic"
+    gen_provider "dynamic" "StdCall" "StdCall" "dynamic"
+    gen_provider "e_sqlite3" "e_sqlite3" "Cdecl" "dllimport"
+    gen_provider "e_sqlcipher" "e_sqlcipher" "Cdecl" "dllimport"
+    gen_provider "sqlite3" "sqlite3" "Cdecl" "dllimport"
+    gen_provider "sqlcipher" "sqlcipher" "Cdecl" "dllimport"
+    gen_provider "winsqlite3" "winsqlite3" "StdCall" "dllimport"
+    gen_provider "internal" "internal" "Cdecl" "dllimport"
+
+    let pack_dirs = [
+        "SQLitePCLRaw.core"
+        "SQLitePCLRaw.ugly" 
+        "SQLitePCLRaw.impl.callbacks" 
+        "SQLitePCLRaw.provider.dynamic" 
+        "SQLitePCLRaw.provider.internal" 
+        "SQLitePCLRaw.provider.e_sqlite3" 
+        "SQLitePCLRaw.provider.e_sqlcipher" 
+        "SQLitePCLRaw.provider.sqlite3" 
+        "SQLitePCLRaw.provider.sqlcipher" 
+        "SQLitePCLRaw.provider.winsqlite3" 
+    ]
+    for s in pack_dirs do
+        exec "dotnet" "pack -c Release" (Path.Combine(top, "src", s))
+
+    let batteries_dirs = [
+        "e_sqlite3.dllimport"
+        "e_sqlite3.dynamic"
+        "e_sqlcipher.dllimport"
+        "e_sqlcipher.dynamic"
+        "sqlite3"
+        "sqlcipher.dynamic"
+        "sqlcipher.dllimport"
+        "winsqlite3"
+        ]
+    for s in batteries_dirs do
+        let dir_name = sprintf "SQLitePCLRaw.batteries_v2.%s" s
+        exec "dotnet" "build -c Release" (Path.Combine(top, "src", dir_name))
+
+    let msbuild_dirs = [
+        "lib.e_sqlite3.android"
+        "lib.e_sqlite3.ios"
+        "lib.e_sqlcipher.android"
+        "lib.e_sqlcipher.ios"
+        "lib.sqlcipher.ios.placeholder"
+        "batteries_v2.e_sqlite3.internal.ios"
+        "batteries_v2.e_sqlcipher.internal.ios"
+        "batteries_v2.sqlcipher.internal.ios"
+        ]
+    for s in msbuild_dirs do
+        let dir_name = sprintf "SQLitePCLRaw.%s" s
+        let dir = (Path.Combine(top, "src", dir_name))
+        // TODO would dotnet restore work instead of nuget.exe here?
+        let nuget_exe = Path.Combine(top, "nuget.exe")
+        let csproj = sprintf "SQLitePCLRaw.%s.csproj" s
+        let nuget_args = sprintf "restore %s" csproj
+        exec nuget_exe nuget_args dir
+        exec "msbuild" "/p:Configuration=Release" dir
+
+    // read the Version from Directory.Build.props
+    let version =
+        let path_xml = Path.Combine(top, "Directory.Build.props")
+        let xml = XElement.Load(path_xml);
+        let props = xml.Elements(XName.Get "PropertyGroup").First()
+        let ver = props.Elements(XName.Get "Version").First()
+        ver.Value
+    printfn "%s" version
+
+    let nuspecs = [
+        "lib.e_sqlite3"
+        "lib.e_sqlcipher"
+        "bundle_green"
+        "bundle_e_sqlite3"
+        "bundle_e_sqlcipher"
+        "bundle_zetetic"
+        "bundle_winsqlite3"
+        ]
+    for s in nuspecs do
+        let nuget_exe = Path.Combine(top, "nuget.exe")
+        let dir_nuspecs = (Path.Combine(top, "nuspecs"))
+        let nuspec_name = sprintf "SQLitePCLRaw.%s.nuspec" s
+        let nuget_args = sprintf "pack -properties version=%s -OutputDirectory %s %s" version dir_nupkgs nuspec_name
+        exec nuget_exe nuget_args dir_nuspecs
+
+    exec "dotnet" "run" (Path.Combine(top, "test_nupkgs", "smoke"))
+
+    exec "dotnet" "test" (Path.Combine(top, "test_nupkgs", "with_xunit"))
+
+    timer.Stop()
+    printfn "Total build time: %A milliseconds" timer.ElapsedMilliseconds
+
+    0 // return an integer exit code
+
