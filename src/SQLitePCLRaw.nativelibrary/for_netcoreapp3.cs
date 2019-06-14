1522f96f889a3d1259c0f40c23ce58bb95716747
Eric Sink <eric@sourcegear.com>
2019-06-14 15:07:59 -0500

    add a dotnetcore3.0 implementation of SQLitePCL.NativeLibrary, which is, of course, a trivial wrapper around System.Runtime.InteropServices.NativeLibrary.

---
+++ src/SQLitePCLRaw.nativelibrary/for_netcoreapp3.cs
@@ -0,0 +1,45 @@
+/*
+   Copyright 2014-2019 SourceGear, LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+
+namespace SQLitePCL
+{
+    public static class NativeLibrary
+    {
+        public static IntPtr Load(string libraryPath)
+        {
+            return System.Runtime.InteropServices.NativeLibrary.Load(libraryPath);
+        }
+        public static IntPtr GetExport(IntPtr handle, string name)
+        {
+            return System.Runtime.InteropServices.NativeLibrary.GetExport(handle, name);
+        }
+        public static void Free(IntPtr handle)
+        {
+            System.Runtime.InteropServices.NativeLibrary.Free(handle);
+        }
+        public static bool TryLoad(string libraryPath, out IntPtr handle)
+        {
+            return System.Runtime.InteropServices.NativeLibrary.TryLoad(libraryPath, out handle);
+        }
+        public static bool TryGetExport(IntPtr handle, string name, out IntPtr address)
+        {
+            return System.Runtime.InteropServices.NativeLibrary.TryGetExport(handle, name, out address);
+        }
+	}
+}
+
