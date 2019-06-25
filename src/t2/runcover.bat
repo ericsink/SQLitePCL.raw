6de3a56085db6b501d86bc85972b29bf5dbbe090
Eric Sink <eric@sourcegear.com>
2019-06-25 15:21:47 -0500

    chg altcover use in src/t2 to opencover, to match the way the other one works

--- src/t2/runcover.bat
+++ src/t2/runcover.bat
@@ -2,6 +2,6 @@ del coverage.xml
 del /Q report\*
 del /Q __Instrumented\*
 dotnet build
-altcover --inputDirectory=.\bin\Debug\netcoreapp2.2
+altcover --inputDirectory=.\bin\Debug\netcoreapp2.2 --opencover
 dotnet .\__Instrumented\tests.dll
 dotnet reportgenerator -reports:coverage.xml -targetdir:report "-assemblyfilters:-xunit.*"
