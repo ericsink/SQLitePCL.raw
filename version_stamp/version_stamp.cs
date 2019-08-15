a77243277f4557396588b5c53ea40751174e7859
Eric Sink <eric@sourcegear.com>
2019-08-15 13:30:42 -0500

    2.0.1

--- version_stamp/version_stamp.cs
+++ version_stamp/version_stamp.cs
@@ -26,7 +26,7 @@ public static class gen
 
     public const int MAJOR_VERSION = 2;
     public const int MINOR_VERSION = 0;
-    public const int PATCH_VERSION = 0;
+    public const int PATCH_VERSION = 1;
 
     // a version string with a -pre-timestamp in it
     public static string NUSPEC_VERSION_PRE_TIMESTAMP = string.Format("{0}.{1}.{2}-pre{3}",
