1fd5f514f449228a2094b66deec19ced72e6b1d3
Eric Sink <eric@sourcegear.com>
2021-06-28 10:21:51 -0500

    comment out cil stuff from the build

--- build/Program.fs
+++ build/Program.fs
@@ -74,7 +74,7 @@ let main argv =
     let pack_dirs = [
         "SQLitePCLRaw.core"
         "SQLitePCLRaw.ugly" 
-        "SQLitePCLRaw.provider.cil" 
+        //"SQLitePCLRaw.provider.cil" 
         "SQLitePCLRaw.provider.dynamic_cdecl" 
         "SQLitePCLRaw.provider.dynamic_stdcall" 
         "SQLitePCLRaw.provider.internal" 
@@ -85,7 +85,7 @@ let main argv =
         exec "dotnet" "pack -c Release" (Path.Combine(top, "src", s))
 
     let batteries_dirs = [
-        "cil"
+        //"cil"
         "e_sqlite3.dllimport"
         "e_sqlite3.dynamic"
         "e_sqlcipher.dllimport"
@@ -151,7 +151,7 @@ let main argv =
         "bundle_zetetic"
         "bundle_winsqlite3"
         "bundle_sqlite3"
-        "bundle_cil"
+        //"bundle_cil"
         ]
     for s in nuspecs do
         let name = sprintf "SQLitePCLRaw.%s" s
@@ -163,7 +163,7 @@ let main argv =
 
     exec "dotnet" "run" (Path.Combine(top, "test_nupkgs", "fsmoke"))
 
-    exec "dotnet" (sprintf "run --framework=%s" "net5.0") (Path.Combine(top, "test_nupkgs", "cil", "fake_xunit"))
+    //exec "dotnet" (sprintf "run --framework=%s" "net5.0") (Path.Combine(top, "test_nupkgs", "cil", "fake_xunit"))
 
     exec "dotnet" "test" (Path.Combine(top, "test_nupkgs", "e_sqlite3", "real_xunit"))
     exec "dotnet" "test" (Path.Combine(top, "test_nupkgs", "winsqlite3", "real_xunit"))
