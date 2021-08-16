90872dec8381d5d94c898cafb646b5f30479e108
Eric Sink <eric@sourcegear.com>
2021-08-16 11:17:12 -0500

    bring back original #430 repro so I can run either one.  bug430_orig uses Microsoft.Data.Sqlite.  bug430_ugly is my attempt to repro the same bug without Microsoft.Data.Sqlite.

---
+++ test_nupkgs/bug430_orig/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection =
+                new SqliteConnection(
+                    @"Data Source=tracker.db"))
+            {
+                connection.Open();
+
+                while (true)
+                {
+                    using (connection.BeginTransaction())
+                    {
+                        var queryCommand = connection.CreateCommand();
+                        queryCommand.CommandText = $@"
+                        SELECT Id, Url, Parent, IsDirectory, IdentifierTag, ContentTag FROM FileTracker WHERE Url=?1;";
+                        queryCommand.Parameters.Add(new SqliteParameter("?1", "file://local/"));
+
+                        var reader = queryCommand.ExecuteReader();
+
+                        var result = HandleReaderSingleDataRow(reader);
+
+                        Console.WriteLine(result?.Id + "");
+                    }
+                }
+            }
+        }
+
+        private static DataRow HandleReaderSingleDataRow(IDataReader reader)
+        {
+            if (!reader.Read())
+            {
+                return null;
+            }
+
+            return new DataRow(
+                reader.GetInt64(0),
+                reader.GetString(1),
+                reader.GetValue(2) as long?,
+                reader.GetBoolean(3),
+                reader.GetValue(4) as string,
+                reader.GetValue(5) as string);
+        }
+
+        public record DataRow(long Id, string Url, long? Parent, bool IsDirectory, string IdentifierTag,
+            string ContentTag);
+    }
+}
+
+
